68105aec71194a77ea8835f6c5026749e5343253
Anand <anandchristal@yahoo.com>
2017-07-14 21:43:59 +0530

    commit repo 1 testing 8

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello testing 6 2World!" );
+        System.out.println( "Hello testing 8 2World!" );
     }
 }
