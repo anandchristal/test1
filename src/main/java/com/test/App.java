188ab2f4cf51719d27ec45800d2f5d3bdcae130f
Anand <anandchristal@yahoo.com>
2017-07-15 10:21:01 +0530

    Update App.java

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello testing 8 2World!" );
+        System.out.println( "Hello testing 8-jenins 2World!" );
     }
 }
