5a18d0fbc479a11d821c50f7b37370b230ae740d
Anand <anandchristal@yahoo.com>
2017-07-17 16:51:48 +0530

    demo to kannan

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello testing 8-jenkin-test1!" );
+        System.out.println( "Hello testing 8-jenkin-test1!-good for demo" );
     }
 }
